b80204029e77137fae9b2d81f1d0d342b1c085c6
sasszem <barath.laci10@gmail.com>
2018-06-29 19:02:26 +0200

    feat(posts): Created PostsController and registered getPost API endpoint
    Created PostsController in app/Http/Controllers/PostsController.php and registered
    getPost API endpoint in routes/api.php
    
    Closes #15

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Posts;
+
+
+class PostsController extends Controller
+{
+    public function byId(Request $request)
+    {
+         $postId = $request->input('id');
+         $post = Posts::with(['index_image','author','author.image','images'])->where('id','=',$postId)->get()->first();
+         if (is_null($post))
+         {
+            return response()->json([],404);
+         }
+         else
+         {
+            return $post;
+         }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('posts')->group(function(){
+    Route::get('getPost','PostsController@byId');
+
+
+});
+
+
