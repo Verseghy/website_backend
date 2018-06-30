468deb04007ebf77c9c3f6004a870ff871d4da72
sasszem <barath.laci10@gmail.com>
2018-06-30 18:22:05 +0200

    feat(posts): Create Authors factory
    Created factory for class App\Models\Posts\Authors in database/factories/AuthorsFactory.php

---
+++ database/factories/AuthorsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\Models\Posts\Authors;
+use App\Models\Posts\Images;
+
+
+$factory->define(Authors::class, function (Faker $faker) {
+    return [
+        'name'=>$faker->name,
+        'description'=>$faker->sentence,
+        'image_id'=>factory(Images::class)->create()->id,
+    ];
+});
