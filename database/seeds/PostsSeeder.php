32bfc10cca95677f54281bee52475b5b2f195a15
sasszem <barath.laci10@gmail.com>
2018-06-30 20:26:56 +0200

    feat(posts): PostsSeeder
    Created DB seeder for posts data in database/seeds/PostsSeeder.php and registered it in
    database/seeds/DatabaseSeeder.php
    
    Closes #3

---
+++ database/seeds/PostsSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\Models\Posts;
+use App\Models\Posts\Labels;
+use App\Models\Posts\Images;
+use App\Models\Posts\Authors;
+
+class PostsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Authors::class,10)->create();
+        factory(Labels::class,20)->create();
+        factory(Posts::class,50)->create()->each(function ($post){
+            $labels = Labels::inRandomOrder()->take(rand(2,7));
+            foreach($labels as $label)
+            {
+                $post->labels()->attach($label);
+            }
+        });
+        
+        factory(Images::class,'postImage',150)->create(); 
+    }
+}
