b9fff86cebcc01c4ce8bcbffc0b1f04cfe070d95
sasszem <barath.laci10@gmail.com>
2018-07-01 01:29:49 +0200

    refactor(posts): Moved all Posts-related factories into one file
    Moved all factories from their own file to database/factories/PostsFactory.php

--- database/factories/PostsFactory.php
+++ database/factories/PostsFactory.php
@@ -5,6 +5,43 @@ use Faker\Generator as Faker;
 use App\Models\Posts;
 use App\Models\Posts\Authors;
 use App\Models\Posts\Images;
+use App\Models\Posts\Labels;
+
+
+
+$factory->define(Images::class, function (Faker $faker) {
+    return [
+        'url' => $faker->imageUrl,
+    ];
+});
+
+$factory->defineAs(Images::class, 'postImage', function (Faker $faker) use ($factory) {
+    $img = $factory->raw('App\Models\Posts\Images');
+    return array_merge($img, [
+        'post_id'=>App\Models\Posts::inRandomOrder()->first()->id,
+    ]);
+});
+
+
+
+$factory->define(Labels::class, function (Faker $faker) {
+    return [
+        'name'=>$faker->word,
+        'color'=>$faker->hexColor,
+    ];
+});
+
+
+
+$factory->define(Authors::class, function (Faker $faker) {
+    return [
+        'name'=>$faker->name,
+        'description'=>$faker->sentence,
+        'image_id'=>factory(Images::class)->create()->id,
+    ];
+});
+
+
 
 $factory->define(Posts::class, function (Faker $faker) {
     return [
