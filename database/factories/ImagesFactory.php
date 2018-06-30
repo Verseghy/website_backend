abafbb6b17ea8018a86a0c7ccfcd4c32b51383c8
sasszem <barath.laci10@gmail.com>
2018-06-30 19:22:41 +0200

    feat(posts): added Images factory for postImages
    Added new factory for class App\Models\Posts\Images in database/factories/ImagesFactory.php,
    which extends the base factory and associates the created Images with a Posts

--- database/factories/ImagesFactory.php
+++ database/factories/ImagesFactory.php
@@ -8,3 +8,10 @@ $factory->define(Images::class, function (Faker $faker) {
         'url' => $faker->url,
     ];
 });
+
+$factory->defineAs(Images::class, 'postImage', function (Faker $faker) use ($factory) {
+    $img = $factory->raw('App\Models\Posts\Images');
+    return array_merge($img, [
+        'post_id'=>App\Models\Posts::inRandomOrder()->first()->id,
+    ]);
+});
