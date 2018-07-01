51d8b4ada651d0cbb1a43128daaae4121685372c
sasszem <barath.laci10@gmail.com>
2018-07-01 14:16:06 +0200

    feat(canteen): Add Canteen DB
    Added canteen DB migration
    
    Closes #29

---
+++ database/migrations/2018_07_01_120118_create_canteen_db.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCanteenDb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('canteen_data', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamp('date');
+        });
+        
+        Schema::create('canteen_menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('menu');
+            $table->unsignedSmallInteger('type');
+        });
+        
+        
+        Schema::create('canteen_pivot_menus_data', function (Blueprint $table) {
+            $table->unsignedInteger('data_id')->nullable();
+            $table->foreign('data_id')->references('id')->on('canteen_data');
+
+            $table->unsignedInteger('menu_id')->nullable();
+            $table->foreign('menu_id')->references('id')->on('canteen_menus');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+            
+            Schema::dropIfExists('canteen_data');
+            Schema::dropIfExists('canteen_menus');
+            Schema::dropIfExists('canteen_pivot_menus_data');
+            
+        Schema::enableForeignKeyConstraints();
+    }
+}
