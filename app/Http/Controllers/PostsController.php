3d49a1548e5316dfc3f044cf43d36c068b44cdc3
sasszem <barath.laci10@gmail.com>
2018-07-01 03:19:54 +0200

    refactor(posts): Add helper methods to PostsController
    Added _resolvedPosts() and _makeThumbnail() helper methods to PostsController

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,10 +7,22 @@ use App\Models\Posts;
 
 class PostsController extends Controller
 {
+    const PAGESIZE = 20;
+
+    private static function _resolvedPosts()
+    {
+        return Posts::with(['index_image','author','author.image','images','labels'])->orderBy('date', 'desc');
+    }
+
+    private static function _makeThumbnail($posts)
+    {
+        return $posts->makeHidden(['images','content']);
+    }
+
     public function byId(Request $request)
     {
         $postId = $request->input('id');
-        $post = Posts::with(['index_image','author','author.image','images','labels'])->where('id', '=', $postId)->get()->first();
+        $post = self::_resolvedPosts()->where('id', '=', $postId)->get()->first();
         if (is_null($post)) {
             return response()->json([], 404);
         } else {
