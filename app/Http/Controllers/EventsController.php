4dae6850e86c822188e213502b2f4ef7d87865d6
Sasszem <Sasszem@users.noreply.github.com>
2018-07-05 16:50:41 +0200

    feat(events): getEventsByMonth API endpoint (#46)
    * feat(events): getEventsByMonth API endpoint
    
    Created EventsController
    Added byMonth() method to EventsController
    Registered API endpoint in routes/api.php
    
    Closes #28
    
    * style: lint
    
    * refactor(events): End date modification
    
    Modified end-date calculation to end the month at next month 1st 00:00:00

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Events;
+
+class EventsController extends Controller
+{
+    public function byMonth(Request $request)
+    {
+        $year = $request->input('year');
+        $month = $request->input('month');
+        
+        $monthStart = mktime(0, 0, 0, $month, 1, $year);
+        $monthEnd = mktime(0, 0, 0, $month+1, 1, $year);
+
+        // Find overlap
+        // https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-two-integer-ranges-for-overlap
+        $overlap = Events::whereDate('date_from', '<=', date('Y-m-d', $monthEnd))->whereDate('date_to', '>=', date('Y-m-d', $monthStart));
+
+        return $overlap->orderBy('date_from')->get();
+    }
+}
