fad1bf941c0e3c1a8c388fec3d08ecd8bcaaed21
Sasszem <barath.laci10@gmail.com>
2018-07-06 18:38:59 +0200

    feat(canteen): Add getCanteenByWeek API endpoint
    If applied, this commit will add 'getCanteenByWeek' API endpoint
    
    Closes #32

--- app/Http/Controllers/CanteensController.php
+++ app/Http/Controllers/CanteensController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 use App\Models\Canteens;
 use App\Models\Canteens\Menus;
@@ -13,5 +14,19 @@ class CanteensController extends Controller
     {
         $type = $request->input('id');
         return Menus::where('type', '=', $type)->orderBy('menu')->get();
+    }
+
+    public function byWeek(Request $request)
+    {
+        $year = $request->input('year');
+        $week = $request->input('week');
+        
+        $date = new Carbon;
+        $date->setIsoDate($year, $week);
+        
+        $start = $date->startOfWeek()->toDateString();
+        $end = $date->endOfWeek()->toDateString();
+        
+        return Canteens::with('menus')->whereDate('date', '>=', $start)->whereDate('date', '<=', $end)->orderBy('date')->get();
     }
 }
