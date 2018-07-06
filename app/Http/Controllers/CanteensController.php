7c6bd61e5f00da4c12ac928ee42c3b634c18bc6a
Sasszem <barath.laci10@gmail.com>
2018-07-06 20:48:31 +0000

    refactor(canteen): HTTP response codes
    If applied, this commit will add parameter and emptiness checks in CanteensController,
    as well as their corresponding HTTP response codes

--- app/Http/Controllers/CanteensController.php
+++ app/Http/Controllers/CanteensController.php
@@ -13,7 +13,23 @@ class CanteensController extends Controller
     public function getMenus(Request $request)
     {
         $type = $request->input('id');
-        return Menus::where('type', '=', $type)->orderBy('menu')->get();
+        
+        if (is_null($type))
+        {
+            return response()->json([], 400);
+        } elseif ($type<0 or $type>2)
+        {
+            return response()->json([], 400);
+        }
+        
+        $menus = Menus::where('type', '=', $type)->orderBy('menu')->get();
+        
+        if ($menus->isEmpty())
+        {
+            return repsponse()->json([], 404);
+        }
+        
+        return $menus;
     }
 
     public function byWeek(Request $request)
@@ -21,12 +37,24 @@ class CanteensController extends Controller
         $year = $request->input('year');
         $week = $request->input('week');
         
+        if (is_null($year) or is_null($week))
+        {
+            return response()->json([], 400);
+        }
+        
         $date = new Carbon;
         $date->setIsoDate($year, $week);
         
         $start = $date->startOfWeek()->toDateString() . ' 00:00:00';
         $end = $date->endOfWeek()->toDateString() .' 00:00:00';
         
-        return Canteens::with('menus')->whereBetween('date', [$start,$end])->orderBy('date')->get();
+        $canteens = Canteens::with('menus')->whereBetween('date', [$start,$end])->orderBy('date')->get();
+        
+        if ($canteens->isEmpty())
+        {
+            return response()->json([], 404);
+        }
+        
+        return $canteens;
     }
 }
