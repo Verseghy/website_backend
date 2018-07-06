688fea3dccbd197f350a3a32d5eec98d2f4ff993
Sasszem <barath.laci10@gmail.com>
2018-07-06 20:35:34 +0000

    refactor(events): HTTP response codes in EventsController
    If applied, this commit will add parameter and emptiness checks in EventsController,
    as well as theit corresponding HTTP response codes

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -13,6 +13,16 @@ class EventsController extends Controller
         $year = $request->input('year');
         $month = $request->input('month');
         
+        if (is_null($year) or is_null($month))
+        {
+            return response()->json([], 400);
+        }
+        
+        if ($month>12 or $month<1)
+        {
+            return response()->json([], 400);
+        }
+        
         $monthStart = mktime(0, 0, 0, $month, 1, $year);
         $monthEnd = mktime(0, 0, 0, $month+1, 1, $year);
 
@@ -20,6 +30,12 @@ class EventsController extends Controller
         // https://stackoverflow.com/questions/3269434/whats-the-most-efficient-way-to-test-two-integer-ranges-for-overlap
         $overlap = Events::whereDate('date_from', '<=', date('Y-m-d', $monthEnd))->whereDate('date_to', '>=', date('Y-m-d', $monthStart));
 
-        return $overlap->orderBy('date_from')->get();
+        $events = $overlap->orderBy('date_from')->get();
+
+        if ($events->isEmpty())
+        {
+            return response()->json([], 404);
+        }
+        return $events;
     }
 }
