493470cf747ab1d5685732839cc8451c9aa31cb4
Sasszem <barath.laci10@gmail.com>
2018-07-06 09:53:41 +0000

    feat(canteen): Add getCanteenMenus API endpoint
    If applied this commit will add 'getCanteenMenus' API endpoint
    
    Closes #33

---
+++ app/Http/Controllers/CanteensController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Canteens;
+use App\Models\Canteens\Menus;
+
+class CanteensController extends Controller
+{
+    public function getMenus(Request $request)
+    {
+        $type = $request->input('id');
+        return Menus::where('type','=',$type)->orderBy('menu')->get();
+    }
+}
