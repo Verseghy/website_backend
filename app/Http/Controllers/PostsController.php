bc3e355e1d2e5b8e7df60a5aaf9e8c30f5df4046
Sasszem <Sasszem@users.noreply.github.com>
2018-07-06 22:58:20 +0200

    refactor(posts): HTTP response codes (#56)
    * refactor(posts): HTTP response codes
    
    If applied, this commit will add parameter checks and emptiness checks,
    as well as their corresponding HTTP response codes to PostsController
    
    * refactor(events): HTTP response codes in EventsController
    
    If applied, this commit will add parameter and emptiness checks in EventsController,
    as well as theit corresponding HTTP response codes
    
    * refactor(canteen): HTTP response codes
    
    If applied, this commit will add parameter and emptiness checks in CanteensController,
    as well as their corresponding HTTP response codes
    
    * style: lint

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -12,32 +12,54 @@ class PostsController extends Controller
     public function byId(Request $request)
     {
         $postId = $request->input('id');
+        if (is_null($postId)) {
+            return response()->json([], 400);
+        }
+        
         $post = self::_resolvedPosts()->where('id', '=', $postId)->get()->first();
         if (is_null($post)) {
             return response()->json([], 404);
-        } else {
-            return $post;
         }
+        return $post;
     }
     
     public function byAuthor(Request $request)
     {
         $authorId = $request->input('id');
-        $posts=self::_resolvedPosts()->where('author_id', '=', $authorId)->paginate(self::PAGESIZE);
-        return self::_makeThumbnail($posts);
+        
+        if (is_null($authorId)) {
+            return response()->json([], 400);
+        }
+        
+        $posts=self::_resolvedPosts()->where('author_id', '=', $authorId);
+        $posts = self::_makeThumbnail($posts);
+        
+        return self::_dataOr404($posts);
     }
+    
     public function listPosts(Request $request)
     {
-        $posts=self::_resolvedPosts()->paginate(self::PAGESIZE);
-        return self::_makeThumbnail($posts);
+        $posts=self::_resolvedPosts();
+        $posts = self::_makeThumbnail($posts);
+        
+        return self::_dataOr404($posts);
     }
+    
     public function byLabel(Request $request)
     {
         $labelId = $request->input('id');
+        
+        if (is_null($labelId)) {
+            return response()->json([], 400);
+        }
+        
         $posts = self::_resolvedPosts()->whereHas('labels', function ($query) use ($labelId) {
             $query->where('id', '=', $labelId);
-        })->paginate(self::PAGESIZE);
-        return self::_makeThumbnail($posts);
+        });
+        
+        $posts = self::_makeThumbnail($posts);
+        
+        return self::_dataOr404($posts);
     }
 
     private static function _resolvedPosts()
@@ -47,6 +69,15 @@ class PostsController extends Controller
 
     private static function _makeThumbnail($posts)
     {
-        return $posts->makeHidden(['images','content']);
+        return $posts->paginate(self::PAGESIZE)->makeHidden(['images','content']);
+    }
+    
+    private static function _dataOr404($data)
+    {
+        if ($data->isEmpty()) {
+            return response()->json([], 404);
+        } else {
+            return $data;
+        }
     }
 }
