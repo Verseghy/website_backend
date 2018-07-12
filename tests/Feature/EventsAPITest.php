c284564b81de89d6d3606876fb639b19a6f95dfd
Sasszem <barath.laci10@gmail.com>
2018-07-12 22:29:58 +0000

    refactor(testing): DB testing in EventsAPITest
    If applied, this commit will add DB testing in class Tests\Feature\EventsAPITest

--- tests/Feature/EventsAPITest.php
+++ tests/Feature/EventsAPITest.php
@@ -6,12 +6,16 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Models\Events;
+use Carbon\Carbon;
+
 class EventsAPITest extends TestsBase
 {
     protected $api = '/api/events';
     
     public function testExample()
     {
+        $this->setupDB();
         $this->getEvents();
     }
     
@@ -19,13 +23,32 @@ class EventsAPITest extends TestsBase
     {
         $endpoint = 'getEventsByMonth';
         
+        $validResponse = array($this->event->toArray());
+        
+        $year = $this->date->year;
+        $month = $this->date->month;
         
         // Valid request
-        $response = $this->API($endpoint, 'year=1999&month=2');
-        $this->assertContains($response->status(), array(404,200));
+        $response = $this->API($endpoint, 'year='.$year.'&month='.$month);
+        $this->assertValidResponse($response, $validResponse);
         
         // Invalid request
         $response = $this->API($endpoint);
         $this->checkResponseCode($response, 400);
+        
+        // Valid request, no data
+        $response = $this->API($endpoint, 'year=1970&month=1');
+        $this->checkResponseCode($response, 404);
+        
+    }
+    
+    public function setupDB()
+    {
+        $this->event = factory(Events::class)->create();    
+        
+        $this->date = new Carbon();
+        $this->event->date_from = $this->date;
+        $this->event->date_to = $this->date;
+        $this->event->save();
     }
 }
