256e26c824d73a44126bf8d951793b0ab95144bf
Sasszem <barath.laci10@gmail.com>
2018-07-16 23:36:44 +0000

    fix(testing): HTTP headers were not properly sent in TestBase@API
    If apllied, this commit will fix the sending of custon HTTP headers in TestBase@API
    via adding 'HTTP_' prefix to them

--- tests/Feature/TestsBase.php
+++ tests/Feature/TestsBase.php
@@ -18,8 +18,15 @@ class TestsBase extends TestCase
         if (substr($params, -1)!=='?') {
             $params = '?'.$params;
         }
-    
-        return $this->call('GET', $root.$url.$params, [], [], [], $headers);
+        
+        $actual_headers = array();
+        
+        foreach($headers as $header => $value)
+        {
+            $actual_headers['HTTP_'.$header] = $value;
+        }
+        
+        return $this->call('GET', $root.$url.$params, [], [], [], $actual_headers);
     }
     
     protected function checkResponseCode($response, $code)
