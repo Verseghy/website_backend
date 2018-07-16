bd3e6d6ea780e0b66c7ce99ecfb643253ab7ca91
Sasszem <barath.laci10@gmail.com>
2018-07-16 23:55:00 +0000

    feat(testing): Add caching tests to EventsAPITest
    If apllied, this commit will add caching-related tests to EventsAPITest

--- tests/Feature/EventsAPITest.php
+++ tests/Feature/EventsAPITest.php
@@ -43,6 +43,20 @@ class EventsAPITest extends TestsBase
         // Valid request, no data
         $response = $this->API($endpoint, 'year=1970&month=1');
         $this->checkResponseCode($response, 404);
+        
+        $farDate = 'Mon, 4 Jan 2100 00:00:00';
+        $oldDate = 'Mon, 5 Jan 1970 00:00:00';
+        
+        
+        // Valid request with if-mod-since header
+        // (new data)
+        $response = $this->API($endpoint, 'year='.$year.'&month='.$month, ['If-modified-since'=>$oldDate]);
+        $this->assertValidResponse($response, $validResponse);
+            
+        // Valid request with if-mod-since header
+        // (not modified)
+        $response = $this->API($endpoint, 'year='.$year.'&month='.$month, ['If-modified-since'=>$farDate]);
+        $this->checkResponseCode($response, 304);
     }
     
     public function setupDB()
