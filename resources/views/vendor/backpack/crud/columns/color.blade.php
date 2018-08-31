617858c554cdb3bd0a624c7a8e16704eb93b3c9a
Sasszem <barath.laci10@gmail.com>
2018-08-31 02:58:17 +0200

    feat(admin): custom 'color' column in admin panel
    If applied, this commit will add a custom column type, 'color' into the admin panel.

---
+++ resources/views/vendor/backpack/crud/columns/color.blade.php
@@ -0,0 +1,14 @@
+{{-- Copied from Backpack source/text column --}}
+
+
+{{-- regular object attribute --}}
+@php
+	$value = data_get($entry, $column['name']);
+	if (is_array($value)) {
+		$value = json_encode($value);
+	}
+@endphp
+
+<span style="color: {{ $value }}">
+	{{ (array_key_exists('prefix', $column) ? $column['prefix'] : '').str_limit(strip_tags($value), array_key_exists('limit', $column) ? $column['limit'] : 50, "[...]").(array_key_exists('suffix', $column) ? $column['suffix'] : '') }}
+</span>
