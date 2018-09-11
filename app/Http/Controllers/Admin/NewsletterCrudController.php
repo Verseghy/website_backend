e2eff8cfb4c6103963346e0a367d7c5474de5424
Sasszem <Sasszem@users.noreply.github.com>
2018-09-11 19:25:44 +0200

    feat: Admin panel for Newsletter (#120)
    * feat: Admin panel for Newsletter
    
    If applied, this commit will add a basic admin panel for Newsletter
    
    Closes #109
    
    * fix: Disable coverage analysis on admin panel for Newsletter
    
    * style: lint
    
    * fix: Typo in NewsletterCrudController
    
    If applied, this commit will fix a typo in NewsletterCrudController
    
    "Copy-pasting is your best friend and your worst enemy"
    
    * fix: Set 'mldata' in newsletter to nullable
    
    * feat: Request rules for Newsletter

---
+++ app/Http/Controllers/Admin/NewsletterCrudController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+
+// VALIDATION: change the requests to match your own file names if you need form validation
+use App\Http\Requests\NewsletterRequest as StoreRequest;
+use App\Http\Requests\NewsletterRequest as UpdateRequest;
+
+/**
+ * Class NewsletterCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read CrudPanel $crud
+ * @codeCoverageIgnore
+ */
+class NewsletterCrudController extends CrudController
+{
+    public function setup()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | CrudPanel Basic Information
+        |--------------------------------------------------------------------------
+        */
+        $this->crud->setModel('App\Models\Newsletter');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/newsletter');
+        $this->crud->setEntityNameStrings('newsletter', 'newsletters');
+
+        /*
+        |--------------------------------------------------------------------------
+        | CrudPanel Configuration
+        |--------------------------------------------------------------------------
+        */
+
+        $this->crud->addField([
+            'name'=>'email',
+            'label'=>'Email',
+            'type'=>'email',
+        ]);
+        
+        $this->crud->addField([
+            'name'=>'token',
+            'label'=>'Token',
+            'type'=>'text',
+        ]);
+        
+        $this->crud->addColumn([
+            'name'=>'email',
+            'label'=>'Email',
+            'type'=>'email',
+        ]);
+
+        // add asterisk for fields that are required in NewsletterRequest
+        $this->crud->setRequiredFields(StoreRequest::class, 'create');
+        $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        // your additional operations before save here
+        $redirect_location = parent::storeCrud($request);
+        // your additional operations after save here
+        // use $this->data['entry'] or $this->crud->entry
+        return $redirect_location;
+    }
+
+    public function update(UpdateRequest $request)
+    {
+        // your additional operations before save here
+        $redirect_location = parent::updateCrud($request);
+        // your additional operations after save here
+        // use $this->data['entry'] or $this->crud->entry
+        return $redirect_location;
+    }
+}
--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -11,5 +11,6 @@ Route::group([
     'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
+    CRUD::resource('newsletter', 'NewsletterCrudController');
     CRUD::resource('events', 'EventsCrudController');
 }); // this should be the absolute last line of this file
