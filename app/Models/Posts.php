e203a647911ee0e57151bd96325ef59e981f7baa
Sasszem <barath.laci10@gmail.com>
2018-09-15 00:38:15 +0200

    refactor: Cleanup Posts

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -47,36 +47,6 @@ class Posts extends Model
 		$this->uploadMultipleFilesToDisk($value, $attribute_name, $disk, $destination_path);
 	}
     
-    // Get a public URL from a path
-    // Stolen from https://stackoverflow.com/questions/37017880/laravel-filesystem-storage-local-public-file-urls-not-working#comment61589587_37018544
-    protected function public_url($disk, $path = '')
-    {
-        $fs = \Storage::disk($disk);
-
-        if ($fs->getAdapter() instanceof \League\Flysystem\Adapter\Local) {
-            return asset($fs->url($path));
-        }
-
-        return $fs->url($path);
-    }
-    
-    
-    // 'Custom' serializer to use accessor for image attribute
-    public function toJson($options=0)
-    {
-        $this->append('index_image');
-        return parent::toJson($options);
-    }
-    
-    // 'Custom' serializer to use accessor for image attribute
-    public function toArray($options=0)
-    {
-        $this->append('index_image');
-        return parent::toArray($options);
-    }
-    // @codeCoverageIgnoreEnd
-    
-    
 	public static function boot()
     {
         parent::boot();
