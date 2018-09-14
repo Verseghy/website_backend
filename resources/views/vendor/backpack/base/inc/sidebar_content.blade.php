eba927bddaa68cf45b4c80f36d8f6689d5774630
Sasszem <barath.laci10@gmail.com>
2018-09-15 00:51:38 +0200

    fix: Broken link in admin panel sidebar
    Removed broken link

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -3,7 +3,6 @@
 <li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
 <li><a href="{{ backpack_url('labels') }}"><i class="fa fa-tag"></i> <span>Manage Labels</span></a></li>
 <li><a href="{{ backpack_url('authors') }}"><i class="fa fa-tag"></i> <span>Manage Authors</span></a></li>
-<li><a href="{{ backpack_url('images') }}"><i class="fa fa-tag"></i> <span>Manage Images</span></a></li>
 <li><a href="{{ backpack_url('posts') }}"><i class="fa fa-tag"></i> <span>Manage Posts</span></a></li>
 <li><a href="{{ backpack_url('newsletter') }}"><i class="fa fa-tag"></i> <span>Manage Newsletter</span></a></li>
 <li><a href="{{ backpack_url('events') }}"><i class="fa fa-tag"></i> <span>Events</span></a></li>
