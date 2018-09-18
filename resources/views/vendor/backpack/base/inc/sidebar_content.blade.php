21f822d0f93039d7497d7253ac9e5b5b344e9349
Sasszem <barath.laci10@gmail.com>
2018-09-18 20:06:37 +0200

    fix: Remove elfinder compleatly

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
 <li><a href="{{ backpack_url('labels') }}"><i class="fa fa-tag"></i> <span>Manage Labels</span></a></li>
 <li><a href="{{ backpack_url('authors') }}"><i class="fa fa-tag"></i> <span>Manage Authors</span></a></li>
 <li><a href="{{ backpack_url('posts') }}"><i class="fa fa-tag"></i> <span>Manage Posts</span></a></li>
