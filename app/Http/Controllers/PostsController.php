d10a0a93857d1ac01e4806e58e46e3ebd5e23c74
Sasszem <barath.laci10@gmail.com>
2019-01-09 03:20:01 +0100

    Feat: Fix and partially re-implement featured post API
    Re-implemented API with DB
    Fixed migrations

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -21,96 +21,78 @@ class PostsController extends Controller
         if (is_null($postId)) {
             return response()->json([], 400);
         }
-        
+
         $post = self::_resolvedPosts()->where('id', '=', $postId)->get()->first();
-        
+
         $maxDate = null;
         if (!is_null($post)) {
             $maxDate=$post->updated_at;
         }
-        
+
         return self::_after($request, $post, $maxDate);
     }
-    
+
     public function listFeaturedPosts(Request $request)
     {
-        $selected_number=0;
-        $selected= array();
-        $posts = self::_resolvedPosts()->orderBy('date','desc')->get();
-        foreach ($posts as $post) {
-            if($post->featured == true and $selected_number<3)
-            {
-                $selected[$selected_number] = $post;
-                $selected_number++;
-            }
-            if($selected_number==3)
-            {
-                break;
-            }
-        }
-        if($selected_number<3)
+
+        $NUMBER_TO_RETURN = 3;
+
+        $result = self::_resolvedPosts()->where('featured','=',true)->orderBy('date','desc')->take($NUMBER_TO_RETURN)->get();
+        $count = $result->count();
+        if($count!==$NUMBER_TO_RETURN)
         {
-            foreach ($posts as $post) {
-                if($post->featured == false and $selected_number<3)
-                {
-                    $selected[$selected_number] = $post;
-                    $selected_number++;
-                }
-                if($selected_number==3)
-                {
-                    break;
-                }
-            }
+            $nonFeatured = self::_resolvedPosts()->where('featured', '=', false)->orderBy('date', 'desc')->take($NUMBER_TO_RETURN - $count)->get();
+            $result = $result->merge($nonFeatured);
         }
-        return $selected;
+        return $result;
 
     }
 
     public function byAuthor(Request $request)
     {
         $authorId = $request->input('id');
-        
+
         if (is_null($authorId)) {
             return response()->json([], 400);
         }
-        
+
         $posts=self::_resolvedPosts()->where('author_id', '=', $authorId);
 
         return self::_after($request, $posts);
     }
-    
+
     public function listPosts(Request $request)
     {
         $posts=self::_resolvedPosts();
-        
+
         return self::_after($request, $posts);
     }
-    
+
     public function byLabel(Request $request)
     {
         $labelId = $request->input('id');
-        
+
         if (is_null($labelId)) {
             return response()->json([], 400);
         }
-        
+
         $posts = self::_resolvedPosts()->whereHas('labels', function ($query) use ($labelId) {
             $query->where('id', '=', $labelId);
         });
-        
-        
+
+
         return self::_after($request, $posts);
     }
-    
+
     public function search(Request $request)
     {
         $searchTerm = $request->input('term');
-        
+
         if (is_null($searchTerm)) {
             return response()->json([], 400);
         }
         $posts = self::_resolvedPosts()->where('content', 'like', '%'.$searchTerm.'%')->orWhere('description', 'like', '%'.$searchTerm.'%')->orWhere('title', 'LIKE', "%$searchTerm%");
-        
+
         return self::_after($request, $posts);
     }
 
@@ -119,12 +101,12 @@ class PostsController extends Controller
         if (is_null($maxDate)) {
             if ($result instanceof \Illuminate\Database\Eloquent\Builder) {
                 $query = $maxDate = $result->latest('updated_at')->first();
-                
+
                 $maxDate = null;
                 if (!is_null($query)) {
                     $maxDate = $query->updated_at;
                 }
-                
+
                 $result = self::_makeThumbnail($result);
             }
         }
@@ -152,7 +134,7 @@ class PostsController extends Controller
 
         $parser = Parsedown::instance()->setBreaksEnabled(true)->setMarkupEscaped(true)->setUrlsLinked(false);
         $post['content'] = isset($post['content']) ? $parser->text($post['content']) : "";
-        
+
         $post['index_image'] = isset($post['index_image']) ? self::_publicUrl($post['index_image']) : null;
         $post['author']['image'] = isset($post['author']['image']) ? self::_publicUrl($post['author']['image'], 'authors_images') : null;
         if (isset($post['images'])) {
@@ -162,7 +144,7 @@ class PostsController extends Controller
         }
         return $post;
     }
-    
+
     private static function _publicUrl($file, $disk='posts_images')
     {
         return asset(\Storage::disk($disk)->url($file));
