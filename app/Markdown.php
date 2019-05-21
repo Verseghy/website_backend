a6ccd3b47c2d411d0a676ef7b35b813ec15f2aac
Sasszem <barath.laci10@gmail.com>
2019-05-21 22:51:53 +0200

    style: lint

--- app/Markdown.php
+++ app/Markdown.php
@@ -1,16 +1,19 @@
 <?php
+
 namespace App;
+
 use Parsedown;
 
 /**
-* Just an inherited parser with disabled lists.
-* Was needed because every article starts with a date 201x. xx. xx
-* And the parser made it a lsit
-* @author     Sasszem
-*/
+ * Just an inherited parser with disabled lists.
+ * Was needed because every article starts with a date 201x. xx. xx
+ * And the parser made it a list.
+ *
+ * @author     Sasszem
+ */
 class Markdown extends Parsedown
 {
-        protected $BlockTypes = array(
+    protected $BlockTypes = array(
         '#' => array('Header'),
         '*' => array('Rule', 'List'),
         '+' => array('List'),
