646b0d9dc885ac24fd502951cbe3256ebae63012
László Baráth <barath.laci10@gmail.com>
2019-05-24 14:16:29 +0200

    fix: disable lists in MD parser (#250)
    * fix: Disable lists in MD parser
    
    * style: lint

---
+++ app/Markdown.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Parsedown;
+
+/**
+ * Just an inherited parser with disabled lists.
+ * Was needed because every article starts with a date 201x. xx. xx
+ * And the parser made it a list.
+ *
+ * @author     Sasszem
+ */
+class Markdown extends Parsedown
+{
+    protected $BlockTypes = array(
+        '#' => array('Header'),
+        '*' => array('Rule', 'List'),
+        '+' => array('List'),
+        '-' => array('SetextHeader', 'Table', 'Rule', 'List'),
+        ':' => array('Table'),
+        '<' => array('Comment', 'Markup'),
+        '=' => array('SetextHeader'),
+        '>' => array('Quote'),
+        '[' => array('Reference'),
+        '_' => array('Rule'),
+        '`' => array('FencedCode'),
+        '|' => array('Table'),
+        '~' => array('FencedCode'),
+    );
+}
