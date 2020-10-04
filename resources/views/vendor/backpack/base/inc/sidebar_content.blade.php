805f5f2d336520a37525f34bb552cacc0d9d003d
Zoltan Szepesi <2dcube01@gmail.com>
2020-10-04 16:01:43 +0200

    feat: add basic permission management

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -47,7 +47,9 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('colleagues') }}"><i class="nav-icon fa fa-user"></i> <span>Colleagues</span></a></li>
 @endif
 
+@if(backpack_user()->hasRole('admin'))
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}\"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i> Logs</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('backup') }}'><i class='nav-icon la la-hdd-o'></i> Backups</a></li>
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('page') }}'><i class='nav-icon fa fa-file-o'></i> <span>Pages</span></a></li>
+@endif
