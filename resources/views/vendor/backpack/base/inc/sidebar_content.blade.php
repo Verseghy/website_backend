54bd82ce4898c1ef389d2fb0b7ec2fe2f9118c35
Zoltan Szepesi <2dcube01@gmail.com>
2020-10-04 03:59:57 +0200

    feat: add logmanager

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -48,3 +48,4 @@
 @endif
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('elfinder') }}\"><i class="nav-icon la la-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('log') }}'><i class='nav-icon la la-terminal'></i> Logs</a></li>
