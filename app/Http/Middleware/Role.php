3d3eec3bd2545a0cd16d0cf8156efd21bb614f62
Zoltan Szepesi <2dcube01@gmail.com>
2020-10-05 20:26:07 +0200

    feat: restrict usage of file manager to admins

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        if (backpack_auth()->guest()) {
+            return redirect('login');
+        }
+
+        if (!backpack_auth()->user()->hasRole($role)) {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
--- config/elfinder.php
+++ config/elfinder.php
@@ -41,7 +41,7 @@ return [
 
     'route' => [
         'prefix' => config('backpack.base.route_prefix', 'admin').'/elfinder',
-        'middleware' => ['web', config('backpack.base.middleware_key', 'admin')], //Set to null to disable middleware filter
+        'middleware' => ['web', config('backpack.base.middleware_key', 'admin'), 'role:admin'], //Set to null to disable middleware filter
     ],
 
     /*
